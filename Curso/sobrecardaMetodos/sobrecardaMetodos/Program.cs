a718cb9ca25a00ba41d856ebe06c4b6d548a774e
Erick <Ikaros__nymph@hotmail.com>
2021-01-14 02:58:16 -0600

    Metodos: Este practica se muestra cual es la sobre carga de metodos

---
+++ Curso/sobrecardaMetodos/sobrecardaMetodos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace sobrecardaMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Visual Studio ayuda a saber que tipo de método de la sobrecarga de métodos vas a
+            // utilizar. Ejemplo;
+
+            Console.WriteLine("llamando al primer método");
+            Console.WriteLine(Suma(7, 8));
+
+            Console.WriteLine("Llamando al segundo método");
+            Console.WriteLine(Suma(7.5, 6.9));
+            Console.WriteLine(Suma(8.9, 8.1));
+
+            Console.WriteLine("Llamando al tercer método");
+            Console.WriteLine(Suma(7, 8, 4));
+        }
+
+        // Aqui es donde se crea la sobrecarga de métodos
+        static int Suma(int operador1, int operador2) => operador1 + operador2;
+
+        static double Suma(double numero1, double numero2) => numero1 + numero2;
+
+        static int Suma(int numero1, int numero2, int numero3) => numero1 + numero2 + numero3;
+        
+        // La sobrecarga de métodos se cuando se crean varios métodos con el mismo nombre, pero
+        // tiene diferentes tipos o parametros como se ve en este ejemplo.
+    }
+}
