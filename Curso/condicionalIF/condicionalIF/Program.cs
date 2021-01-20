0df4f1b1d0f4930d04c6a221a93d7eea7ebf756e
Erick <Ikaros__nymph@hotmail.com>
2021-01-20 02:27:52 -0600

    Concicionales: comienzo con declaracion de variables bloleanas * operadores * Ejemplo del operador IF

---
+++ Curso/condicionalIF/condicionalIF/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace condicionalIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+
+            Console.WriteLine("Evaluaremos si eres mayor de edad");
+            Console.WriteLine("Por favor ingresa tú edad:");
+            edad = int.Parse(Console.ReadLine());
+
+            if (edad >=18)
+            {
+                Console.WriteLine("Felicidades eres mayor de edad!");
+            }
+            else
+            {
+                Console.WriteLine("Aun eres menor de edad.");
+            }
+
+        }
+    }
+}
