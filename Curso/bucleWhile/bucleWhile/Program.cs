14ae41393ba5dde37dbe2b9f05ba88451df6fbda
Erick <Ikaros__nymph@hotmail.com>
2021-01-26 03:16:49 -0600

    BUCLES: estructura y ejemplo del bucle WHILE

---
+++ Curso/bucleWhile/bucleWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace bucleWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deseas ingresar al bucle?");
+            string respuesta = Console.ReadLine();
+
+            while (respuesta != "no")
+            {
+                Console.WriteLine("Estas dentro del bucle while");
+                Console.WriteLine("Ingresa tu nombre, por favor.");
+                string nombre = Console.ReadLine();
+                Console.WriteLine($"Saldras del buble {nombre} si escribes la palabra 'no'");
+                respuesta = Console.ReadLine();
+            }
+
+            Console.WriteLine("Haz salido del bucle");
+        }
+    }
+}
---
+++ Curso/ejercicio1While/ejercicio1While/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ejercicio1While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear un programa que de un número al azar del 0-100, y que nos indique si
+            // estamos sercas o no del numero y una vez localizado el número nos indique las 
+            // n cantidad de veces que lo intentamos.
+
+            Random numero = new Random();
+            int numeroAleatorio = numero.Next(0, 100);
+            Console.WriteLine(numeroAleatorio);
+        }
+    }
+}
