897a42758c35c25b7676deed6a3f887c4af50c09
Erick <Ikaros__nymph@hotmail.com>
2021-01-27 04:05:12 -0600

    Ejercicio resuelto con menos codigo...

--- Curso/ejercicio1While/ejercicio1While/Program.cs
+++ Curso/ejercicio1While/ejercicio1While/Program.cs
@@ -15,8 +15,11 @@ namespace ejercicio1While
             int numeroAleatorio = numero.Next(0, 100);
             Console.WriteLine(numeroAleatorio);
 
-            //Practica hecha por mi
+            // Practica hecha por mi
             miPractica();
+
+            // Practica resuelta con menos código
+            EjercicioResuleto();
         }
         static void miPractica()
         {
@@ -49,6 +52,28 @@ namespace ejercicio1While
             Console.WriteLine("Felicidades encontraste el numero");
             Console.WriteLine(numeroAleatorio);
             Console.WriteLine($"Lo intentaste: {contador} veces");
+        }
+
+        static void EjercicioResuleto()
+        {
+            Random numero = new Random();
+
+            int aleatorio = numero.Next(0,100);
+
+            int miNumero = 101;
+            int intentos = 0;
+
+            Console.WriteLine("Introduce un número 0 y 100");
+
+            while (miNumero != aleatorio)
+            {
+                intentos++;
+
+                miNumero = int.Parse(Console.ReadLine());
+                if (miNumero > aleatorio) Console.WriteLine("El número es más bajo");
+                if (miNumero < aleatorio) Console.WriteLine("El número es más alto");
+            }
+            Console.WriteLine($"Correcto! Haz necesitano {intentos} intentos.");
         }
     }
 }
