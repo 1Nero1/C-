c68bcc004c4a78a4797a5753b263a7bdbcf49bda
Erick <Ikaros__nymph@hotmail.com>
2021-02-05 02:47:33 -0600

    Excepciones: Muestra y uso del Checked y como visual studios puede abrir una funcion sin la necesidad de usar Checked en el codigo...

---
+++ Curso/checked_unchecked/checked_unchecked/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace checked_unchecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejemplo del uso de Unchecked
+            EjemploSinchecked();
+
+            EjemploConChecked();
+        }
+
+        static void EjemploSinchecked()
+        {
+            // Ejemplo sin un checked 
+            int numero = int.MaxValue;
+            Console.WriteLine("Este es el número maximo de un INT "+numero);
+
+            int suma = numero + 15;
+
+            Console.WriteLine("sumamos el maximo valor +15 "+suma);
+
+            // como vemos nos dio un número negativo ya que C# sabe que esta mal el 
+            // resultado pero para no dejar caer el programa, manda un resultado
+            // erroneo y continua
+            Console.WriteLine("más código...");
+        }
+        static void EjemploConChecked()
+        {
+            int numero = int.MaxValue;
+            Console.WriteLine("Este es el número maximo de un INT " + numero);
+
+            checked
+            {
+                int suma = numero + 15;
+                Console.WriteLine("sumamos el maximo valor +15 " + suma);
+                // Como vemos aqui, Si, se detiene el programa y no continua con las 
+                // demas lineas de código. ya que aqui si esta verificando (estrictamente)
+                // las lineas de código...
+            }
+
+            Console.WriteLine("Más código...");
+        }
+    }
+}
